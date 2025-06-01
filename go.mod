d2ebe48f654f77011c97a0c89a9b440aba0dcbc8
Cooper Terrill <138729166+cooperaterrill@users.noreply.github.com>
2025-06-01 15:15:29 -0500

    feat : main gameplay loop

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/cooperaterrill/zetatrack
+
+go 1.24.3
+
+require (
+	github.com/Knetic/govaluate v3.0.0+incompatible // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/term v0.32.0 // indirect
+)
---
+++ zetatrack.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"syscall"
+	"time"
+
+	"math/rand/v2"
+
+	"github.com/Knetic/govaluate"
+	"golang.org/x/term"
+)
+
+type Problem struct {
+	FirstNum  int
+	Operation string
+	SecondNum int
+}
+
+const GameloopTime = 10
+const ClearSignal = "clear"
+
+func readInput(buf []byte, channel chan string) {
+	var answerBuf = make([]byte, 10)
+	answerBufFront := 0
+	for {
+		select {
+		case x, ok := <-channel:
+			if ok {
+				if x == ClearSignal {
+					answerBufFront = 0
+				}
+			} else {
+				fmt.Printf("Something went wrong!")
+			}
+		default:
+		}
+		n, err := os.Stdin.Read(buf)
+		if err == nil && n > 0 {
+			//fmt.Printf("Read: %q", buf[0])
+			if buf[0] == 0x7f {
+				if answerBufFront <= 0 {
+					continue
+				}
+				answerBufFront--
+				fmt.Printf("\b \b")
+			} else {
+				if answerBufFront >= len(answerBuf) {
+					continue
+				}
+				answerBuf[answerBufFront] = buf[0]
+				answerBufFront++
+			}
+			fmt.Printf("%c", buf[0])
+			//fmt.Printf("Current answer: %s\n", answerBuf[0:answerBufFront])
+			channel <- string(answerBuf[0:answerBufFront])
+		}
+		time.Sleep(time.Millisecond * 5)
+	}
+}
+
+func randRange(min int, max int) int {
+	return rand.IntN(max-min+1) + min
+}
+
+func genProblem(firstNumMinValue int, firstNumMaxValue int, legalOps []string, secondNumMinValue int, secondNumMaxValue int) Problem {
+	var problem Problem
+	problem.FirstNum = randRange(firstNumMinValue, firstNumMaxValue)
+	problem.SecondNum = randRange(secondNumMinValue, secondNumMaxValue)
+	problem.Operation = legalOps[randRange(0, len(legalOps)-1)]
+
+	return problem
+}
+
+func main() {
+	timer := time.NewTimer(GameloopTime * time.Second)
+
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic(err)
+	}
+
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	fd := int(os.Stdin.Fd())
+	var buf = make([]byte, 1)
+
+	err = syscall.SetNonblock(fd, true)
+	if err != nil {
+		panic(err)
+	}
+
+	score := 0
+	go func() {
+		<-timer.C
+		fmt.Printf("\r\nScore: %d\r\n", score)
+		os.Exit(0)
+	}()
+
+	inputChannel := make(chan string)
+	go readInput(buf, inputChannel)
+
+	firstProblem := true
+	for {
+		problem := genProblem(1, 12, []string{"*", "+"}, 1, 99)
+		problemString := fmt.Sprintf("%d %s %d", problem.FirstNum, problem.Operation, problem.SecondNum)
+		if firstProblem {
+			fmt.Printf("%s: ", problemString)
+			firstProblem = false
+		} else {
+			fmt.Printf("\r\n%s: ", problemString)
+		}
+		expression, err := govaluate.NewEvaluableExpression(problemString)
+		if err != nil {
+			fmt.Print("Error parsing expression\r\n")
+			panic(err)
+		}
+		evaluated, err := expression.Evaluate(nil)
+		if err != nil {
+			fmt.Print("Error evaluating expression\r\n")
+		}
+		questionAns := int(evaluated.(float64))
+		for {
+			userAns := <-inputChannel
+			if userAns == strconv.Itoa(questionAns) {
+				//fmt.Printf("\r\nYou got the right answer\r\n")
+				score++
+				inputChannel <- ClearSignal
+				break
+			}
+		}
+	}
+}
